630ba9b1552f9912b12c9bfd592f0bf29e31073b
Lydia Gregory <ljg2gb@virginia.edu>
2020-07-14 09:13:43 -0600

    isDivisible Codewars kata w/ refactored code

--- index.js
+++ index.js
@@ -2,6 +2,20 @@
 
 // 6/25/20
 function positiveSum(arr) {
-    const pos = arr.filter(num => num > 0)
-    return pos.length ? pos.reduce((num, sum) => sum + num) : 0 
-  }
+  const pos = arr.filter(num => num > 0)
+  return pos.length ? pos.reduce((num, sum) => sum + num) : 0 
+}
+
+// 7/14/20
+function isDivisible(n, x, y) {
+  return (n % x === 0 && n % y === 0) 
+  ? true 
+  : false
+}
+
+// 7/14/20 refactored
+// Because the ternary condition evaluates to true or false, we don't need the ternary
+function isDivisible(n, x, y) {
+  return (n % x === 0 && n % y === 0)
+}
+
