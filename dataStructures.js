21e6f5875a49265ea28cc511bbb5fed97fb1db5a
Lydia Gregory <ljg2gb@virginia.edu>
2020-09-08 13:21:25 -0400

    rotate array by n

--- dataStructures.js
+++ dataStructures.js
@@ -264,4 +264,12 @@ function unrollAMatrix(matrix) {
     return unrolled.flat()
 }
 
-console.log(unrollAMatrix([[1,2,3,4],[12,13,14,5],[11,16,15,6],[10,9,8,7]]))
+// console.log(unrollAMatrix([[1,2,3,4],[12,13,14,5],[11,16,15,6],[10,9,8,7]]))
+
+// challenge 8: rotate array by N
+function rightRotate(arr,n) {
+    const shift = arr.splice(arr.length -n)
+    return [...shift, ...arr]
+}
+
+console.log(rightRotate([300,-1,3,0],3))
